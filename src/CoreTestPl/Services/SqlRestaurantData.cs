b9bd3c6202583635ca14055e15ad576386a25607
Daziko <kozikdaniel@gmail.com>
2017-04-13 23:25:19 +0200

    Add entity framework

---
+++ src/CoreTestPl/Services/SqlRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreTestPl.Entities;
+
+namespace CoreTestPl.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        readonly OdeToFoodDbContext context;
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+
+        }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            context.Add(newRestaurant);
+            context.SaveChanges();
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return context.Restaurants;
+        }
+    }
+}
