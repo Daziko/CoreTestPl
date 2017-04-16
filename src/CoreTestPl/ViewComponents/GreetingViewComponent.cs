ad08300d38da94e217c5f672383e516b368a0756
Daziko <kozikdaniel@gmail.com>
2017-04-16 21:15:03 +0200

    Add View Component

---
+++ src/CoreTestPl/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using CoreTestPl.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreTestPl.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        readonly IGreeter greeder;
+
+        public GreetingViewComponent(IGreeter greeder)
+        {
+            this.greeder = greeder;
+        }
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = greeder.GetGreeting();
+            return Task.FromResult<IViewComponentResult>(View("Default", model));
+        }
+    }
+}
