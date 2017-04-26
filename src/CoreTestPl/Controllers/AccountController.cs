c7911f9b44078277071af0127087920b254952cc
Daziko <kozikdaniel@gmail.com>
2017-04-26 16:25:50 +0200

    Add registration feature

---
+++ src/CoreTestPl/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using CoreTestPl.Entities;
+using CoreTestPl.WievModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTestPl.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User()
+                {
+                    UserName = model.Username
+                };
+                var createResult = await userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var identityError in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", identityError.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
